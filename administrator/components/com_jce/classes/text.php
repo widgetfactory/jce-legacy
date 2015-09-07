821db6cb7edeaf512eff34e657324bb2572036b9
Ryan Demmer <ryandemmer@gmail.com>
2015-09-07 09:30:14 +0100

    Re-format

--- administrator/components/com_jce/classes/text.php
+++ administrator/components/com_jce/classes/text.php
@@ -9,56 +9,54 @@
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
 defined('_JEXEC') or die('RESTRICTED');
 
-abstract class WFText
-{
-	/**
-	 * Transalate a language string.
-	 * @param $string 	Language string
-	 * @param $default 	Default
-	 */
-	public static function _($string, $default = '')
-	{
-		$language = JFactory::getLanguage();
+abstract class WFText {
 
-		// replace legacy JCE_ prefix
-		$string 	= str_replace('JCE_', 'WF_', $string);		
-		$translated = $language->_($string);
-		
-		if ($translated == $string) {
-			if ($default) {
-				return $default;
-			}
-			
-			if (strpos($string, 'WF_') !== false) {
-				$view = JRequest::getWord('view', '');
-				// remove prefix
-				$translated = preg_replace(array('#^(WF_)#', '#(LABEL|OPTION|FILEGROUP|' . strtoupper($view) . ')_#', '#_(DESC|TITLE)#'), '', $string);			
-				$translated = ucwords(strtolower(str_replace('_', ' ', $translated)));
-			}
-		}
-		
-		return $translated;
-	}
-	
-	/**
-	 * Translate a string with variables
-	 * @param string $string
-	 * @copyright 	Copyright (c) 2005 - 2007 Open Source Matters. All rights reserved.
-	 */
-	public static function sprintf($string)
-	{
-		$language = JFactory::getLanguage();
-		
-		$args = func_get_args();
-		
-		if (count($args) > 0) {
-			$args[0] = $language->_($args[0]);
-			return call_user_func_array('sprintf', $args);
-		}
-		
-		return '';
-	}
+    /**
+     * Transalate a language string.
+     * @param $string 	Language string
+     * @param $default 	Default
+     */
+    public static function _($string, $default = '') {
+        $language = JFactory::getLanguage();
+
+        // replace legacy JCE_ prefix
+        $string = str_replace('JCE_', 'WF_', $string);
+        $translated = $language->_($string);
+
+        if ($translated == $string) {
+            if ($default) {
+                return $default;
+            }
+
+            if (strpos($string, 'WF_') !== false) {
+                $view = JRequest::getWord('view', '');
+                // remove prefix
+                $translated = preg_replace(array('#^(WF_)#', '#(LABEL|OPTION|FILEGROUP|' . strtoupper($view) . ')_#', '#_(DESC|TITLE)#'), '', $string);
+                $translated = ucwords(strtolower(str_replace('_', ' ', $translated)));
+            }
+        }
+
+        return $translated;
+    }
+
+    /**
+     * Translate a string with variables
+     * @param string $string
+     * @copyright 	Copyright (c) 2005 - 2007 Open Source Matters. All rights reserved.
+     */
+    public static function sprintf($string) {
+        $language = JFactory::getLanguage();
+
+        $args = func_get_args();
+
+        if (count($args) > 0) {
+            $args[0] = self::_($args[0]);
+            return call_user_func_array('sprintf', $args);
+        }
+
+        return '';
+    }
+
 }
