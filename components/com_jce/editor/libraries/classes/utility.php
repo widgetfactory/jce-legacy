22e8fe966f53efe527e76a1bff9bb25e7570a886
Ryan Demmer <ryandemmer@gmail.com>
2015-10-26 09:55:38 +0000

    Remove tag check

--- components/com_jce/editor/libraries/classes/utility.php
+++ components/com_jce/editor/libraries/classes/utility.php
@@ -409,20 +409,6 @@ abstract class WFUtility
             fclose($fp);
         }
 
-        // check for HTML code in image files for XSS attack in IE < 8 : TODO - Remove in future?
-        if ($isImage) {
-            $tags = 'a,abbr,acronym,address,area,b,base,bdo,big,blockquote,body,br,button,caption,cite,code,col,colgroup,dd,del,dfn,div,dl,dt,em,fieldset,form,h1,h2,h3,h4,h5,h6,head,hr,html,i,img,input,ins,kbd,label,legend,li,link,map,meta,noscript,object,ol,optgroup,option,p,param,pre,q,samp,script,select,small,span,strong,style,sub,sup,table,tbody,td,textarea,tfoot,th,thead,title,tr,tt,ul,var';
-            $data = file_get_contents($file['tmp_name'], false, null, -1, 256);
-
-            foreach (explode(',', $tags) as $tag) {
-                // check for tag eg: <body> or <body
-                if (stristr($data, '<' . $tag . ' ') || stristr($data, '<' . $tag . '>')) {
-                    @unlink($file['tmp_name']);
-                    throw new InvalidArgumentException('Upload Failed: The file contains HTML code.');
-                }
-            }
-        }
-
         // validate image
         if ($isImage && @getimagesize($file['tmp_name']) === false) {
             @unlink($file['tmp_name']);
