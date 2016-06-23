622f2a34cd7c46357f55aa764892c21918f58b51
Ryan <ryandemmer@gmail.com>
2016-06-23 09:51:20 +0100

    Update clipboard plugin

--- components/com_jce/editor/tiny_mce/plugins/clipboard/tmpl/default.php
+++ components/com_jce/editor/tiny_mce/plugins/clipboard/tmpl/default.php
@@ -2,7 +2,7 @@
 
 /**
  * @package   	JCE
- * @copyright 	Copyright (c) 2009-2016 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright (c) 2009-2015 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -13,11 +13,11 @@
 defined( 'WF_EDITOR' ) or die('RESTRICTED');
 ?>
 <form onsubmit="return false;" action="#">
-	<div id="title" class="title"><?php echo WFText::_('WF_PASTE_TITLE');?></div>		
+	<h5 id="title"><?php echo WFText::_('WF_PASTE_TITLE');?></h5>
 	<div id="container"></div>
-	
-	<div class="mceActionPanel">
-	<button id="insert" ><?php echo WFText::_('WF_LABEL_INSERT');?></button>
+
+	<div class="actionPanel">
+	<button id="insert"><?php echo WFText::_('WF_LABEL_INSERT');?></button>
 	<button id="cancel"><?php echo WFText::_('WF_LABEL_CANCEL');?></button>
 	</div>
 </form>
