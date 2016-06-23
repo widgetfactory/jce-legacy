f24484171ec93aea3f9a7a9b17adb8f20d3256ce
Ryan <ryandemmer@gmail.com>
2016-06-23 09:52:17 +0100

    Update fontselect to use defaults if no value set

--- components/com_jce/editor/tiny_mce/plugins/fontselect/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/fontselect/editor_plugin.js
@@ -11,7 +11,7 @@
     var DOM = tinymce.DOM, Event = tinymce.dom.Event, extend = tinymce.extend, each = tinymce.each, Cookie = tinymce.util.Cookie, explode = tinymce.explode;
 
     tinymce.create('tinymce.plugins.FontSelectPlugin', {
-        
+
         fonts : "Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats",
 
         init: function(ed, url) {
@@ -26,7 +26,7 @@
                     if (!fn && n.style.fontFamily) {
                         fn = n.style.fontFamily.replace(/[\"\']+/g, '').replace(/^([^,]+).*/, '$1').toLowerCase();
                     }
-                    
+
                     c.select(function(v) {
                         return v.replace(/^([^,]+).*/, '$1').toLowerCase() == fn;
                     });
@@ -35,13 +35,11 @@
         },
         createControl: function(n, cf) {
             var ed = this.editor;
-            
+
             switch (n) {
                 case "fontselect":
-                    
-                    if (ed.getParam('fontselect_fonts')) {
-                        return this._createFontSelect();
-                    }
+
+                    return this._createFontSelect();
 
                     break;
             }
@@ -75,8 +73,7 @@
             });
 
             if (c) {
-                
-                each(ed.getParam('fontselect_fonts', '', 'hash'), function(v, k) {
+                each(ed.getParam('fontselect_fonts', self.fonts, 'hash'), function(v, k) {
                     if (/\d/.test(v)) {
                         v = "'" + v + "'";
                     }
