c3cc9662fc916036d36e9608480c0843444f43fe
Ryan <ryandemmer@gmail.com>
2016-06-23 09:56:47 +0100

    Add caller support and new filter options

--- components/com_jce/editor/tiny_mce/plugins/browser/classes/browser.php
+++ components/com_jce/editor/tiny_mce/plugins/browser/classes/browser.php
@@ -2,7 +2,7 @@
 
 /**
  * @package   	JCE
- * @copyright 	Copyright (c) 2009-2016 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright (c) 2009-2015 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -13,50 +13,47 @@ defined('_JEXEC') or die('RESTRICTED');
 
 require_once(WF_EDITOR_LIBRARIES . '/classes/manager.php');
 
-final class WFFileBrowserPlugin extends WFMediaManager {
+class WFFileBrowserPlugin extends WFMediaManager {
     /*
      * @var string
      */
-
     protected $_filetypes = 'word=doc,docx;powerpoint=ppt,pptx;excel=xls,xlsx;image=gif,jpeg,jpg,png;acrobat=pdf;archive=zip,tar,gz;flash=swf;winrar=rar;quicktime=mov,mp4,qt;windowsmedia=wmv,asx,asf,avi;audio=wav,mp3,aiff;openoffice=odt,odg,odp,ods,odf;text=rtf,txt,csv';
 
-    /**
-     * @access	protected
-     */
-    public function __construct() {
-        parent::__construct();
+    public function __construct($config = array()) {
+        $config = array(
+          'layout' => 'browser',
+          'can_edit_images' => 1,
+          'show_view_mode' => 1
+        );
 
-        $browser = $this->getBrowser();
+        parent::__construct($config);
 
-        if (JRequest::getWord('type', 'file') == 'file') {
-            // Add all files
-            $browser->addFileTypes(array('WF_FILEGROUP_ALL' => '*.*'));
+        // get the plugin that opened the file browser
+        $caller     = JRequest::getWord('caller', 'browser');
+        $filter     = JRequest::getVar('filter', 'files');
+
+        // clean filter value
+        $filter = (string) preg_replace('/[^\w_,]/i', '', $filter);
+
+        if ($filter == 'images') {
+            $filetypes = 'images=jpg,jpeg,png,gif';
+        } else if ($filter === 'media') {
+            $filetypes = 'windowsmedia=avi,wmv,wm,asf,asx,wmx,wvx;quicktime=mov,qt,mpg,mpeg,m4a;flash=swf;shockwave=dcr;real=rm,ra,ram;divx=divx;video=mp4,ogv,ogg,webm,flv,f4v;audio=mp3,ogg,wav;silverlight=xap';
+        } else if ($filter === 'html') {
+            $filetypes = 'html=html,htm,txt';
         } else {
-            $browser->setFileTypes('images=jpg,jpeg,png,gif');
-        }
-
-        $filter = JRequest::getString('filter');
-
-        if ($filter) {
-            if ($filter === 'images') {
-                $filetypes = 'images=jpg,jpeg,png,gif';
-            } else if ($filter === 'media') {
-                $filetypes = 'windowsmedia=avi,wmv,wm,asf,asx,wmx,wvx;quicktime=mov,qt,mpg,mpeg,m4a;flash=swf;shockwave=dcr;real=rm,ra,ram;divx=divx;video=mp4,ogv,ogg,webm,flv,f4v;audio=mp3,ogg,wav;silverlight=xap';
-            } else if ($filter === 'html') {
-                $filetypes = 'html=html,htm,txt';
+            if (strpos($filter, ',') !== false) {
+                $filetypes = 'custom=' . $filter;
             } else {
-                // custom filter list, eg: jpg,jpeg,png,pdf
-                if (strpos($filter, ',') !== false) {
-                  $filetypes = 'files=' . $filter;
-                } else {
-                  $filetypes = $this->get('_filetypes');
-                }
+                $filetypes = $this->get('_filetypes');
             }
-
-            $browser->setFileTypes($filetypes);
         }
-        // remove insert button
-        $browser->removeButton('file', 'insert');
+
+        // get filetypes from params
+        $filetypes = $this->getParam($caller.'.extensions', $filetypes);
+
+        // set filetypes
+        $this->setFileTypes($filetypes);
     }
 
     /**
@@ -66,13 +63,18 @@ final class WFFileBrowserPlugin extends WFMediaManager {
     public function display() {
         parent::display();
 
+        if (JRequest::getCmd('dialog') === "editor") {
+          return;
+        }
+
         $document = WFDocument::getInstance();
         $settings = $this->getSettings();
 
         $document->addScript(array('browser'), 'plugins');
 
         if ($document->get('standalone') == 1) {
-            $document->addScript(array('browser'), 'component');
+            $document->addScript(array('browser.min'), 'component');
+            $document->addStyleSheet(array('browser.min'), 'component');
 
             $element = JRequest::getCmd('element', JRequest::getCmd('fieldid', ''));
 
@@ -81,8 +83,8 @@ final class WFFileBrowserPlugin extends WFMediaManager {
                     'root' => JURI::root(),
                     'site' => JURI::base(true) . '/'
                 ),
-                'manager' => $settings,
-                'element' => $element
+                'filebrowser' => $settings,
+                'element'     => $element
             );
 
             $document->addScriptDeclaration('jQuery(document).ready(function($){$.WFBrowserWidget.init(' . json_encode($options) . ');});');
@@ -92,5 +94,3 @@ final class WFFileBrowserPlugin extends WFMediaManager {
         }
     }
 }
-
-?>
