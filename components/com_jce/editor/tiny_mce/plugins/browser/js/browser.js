0bb889a1f01b0a8a89ad34091dd946cf59426a94
Ryan <ryandemmer@gmail.com>
2016-06-23 09:56:24 +0100

    Update browser plugin to remove old callback method

--- components/com_jce/editor/tiny_mce/plugins/browser/js/browser.js
+++ components/com_jce/editor/tiny_mce/plugins/browser/js/browser.js
@@ -1,95 +1,98 @@
 /**
  * @package   	JCE
- * @copyright 	Copyright (c) 2009-2016 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright (c) 2009-2015 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-var BrowserDialog = {
-	
-    settings : {},
-	
-    init : function(ed) {
-        var self = this, action = "insert";
-		
-        $('button#insert').click(function(e) {
-            BrowserDialog.insert();
-            e.preventDefault();
-        });
-		
-        tinyMCEPopup.resizeToInnerSize();
+(function($) {
+  var BrowserDialog = {
 
-        var win = tinyMCEPopup.getWindowArg("window");        
-        var src = tinyMCEPopup.getWindowArg("url");
-
-        $.Plugin.init();
-	
-        if (/(:\/\/|www|index.php(.*)\?option)/gi.test(src)) {
-            src = '';	
-        }
-        
-        if (src) {
-            src = tinyMCEPopup.editor.convertURL(src);
-            $('#insert').button('option', 'label', tinyMCEPopup.getLang('update', 'Update', true));
-        }
-		
-        $('<input type="hidden" id="src" value="'+ src +'" />').appendTo(document.body);
-	
-        WFBrowserWidget.init({        	
-            onFileClick : function(e, file) {
-                self.selectFile(file);
-            },
-
-            onFileInsert : function(e, file) {
-                self.selectFile(file);
-            },
-            
-            expandable : false
-        });
+    settings: {
+      "filebrowser" : {}
     },
-	
-    insert : function() {
-        var win = tinyMCEPopup.getWindowArg("window");
-        
-        var src = $('#src').val();
-        
-        if (src === "") {
-            var selected = WFFileBrowser.getSelectedItems();
-            
-            if (selected.length) {
-                this.selectFile(selected[0]);
-                
-                src = $('#src').val();
-            }
+
+    init: function(ed) {
+      var self = this,
+        action = 'insert';
+
+      $('button#insert').click(function(e) {
+        BrowserDialog.insert();
+        e.preventDefault();
+      });
+
+      var win = tinyMCEPopup.window;
+      var src = tinyMCEPopup.getWindowArg('value');
+
+      $.Plugin.init();
+
+      if (/(:\/\/|www|index.php(.*)\?option)/gi.test(src)) {
+        src = '';
+      }
+
+      if (src) {
+        src = tinyMCEPopup.editor.convertURL(src);
+        $('.ui-button-text', '#insert').text(tinyMCEPopup.getLang('update', 'Update', true));
+      }
+
+      $('#src').val(src).filebrowser().on('filebrowser:onfileclick', function(e, file) {
+          self.selectFile(file);
+      });
+    },
+
+    insert: function() {
+      var win = tinyMCEPopup.getWindowArg('window'), self = this;
+
+      var src = $('#src').val(), selected = [];
+      var callback = tinyMCEPopup.getWindowArg('callback');
+
+      // get selected items
+      $('#src').trigger('filebrowser:insert', function(selected, data) {
+
+        if (typeof callback === "string") {
+          if (src === '' && data.length) {
+            self.selectFile(data[0]);
+          }
+
+          win.document.getElementById(callback).value = $('#src').val();
         }
 
-        // insert information now
-        win.document.getElementById(tinyMCEPopup.getWindowArg("input")).value = src;
+        if (typeof callback === "function") {
+          callback(selected, data);
+        }
 
         // close popup window
         tinyMCEPopup.close();
+      });
     },
-	
-    selectFile : function(file) {
-        var self 	= this;
-        var name 	= file.title;
 
-        var src = $(file).data('url');
+    selectFile: function(file) {
+      var self = this, name = file.title, src = file.url;
 
-        src = src.charAt(0) == '/' ? src.substring(1) : src;			
+      if (src) {
+        // remove leading slash
+        src = src.replace(/^\//, '');
         $('#src').val(src);
+      }
     }
-};
+  };
 
-var WFBrowserWidget = {
+  var WFBrowserWidget = {
     init: function(settings) {
-        // Create File Browser
-        WFFileBrowser.init('#src', settings);
+      // Create File Browser
+      $('#src').filebrowser();
     }
-};
+  };
 
-if (typeof tinyMCEPopup !=="undefined") {
-    tinyMCEPopup.onInit.add(BrowserDialog.init, BrowserDialog);
-}
+  if (typeof tinyMCEPopup !== "undefined") {
+    $(document).ready(function() {
+        BrowserDialog.init();
+    });
+  }
+
+  window.WFBrowserWidget  = WFBrowserWidget;
+  window.BrowserDialog    = BrowserDialog;
+
+})(jQuery);
