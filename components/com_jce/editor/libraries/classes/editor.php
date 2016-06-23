313b738925190cbfda88aa16cc97c61b1eee131a
Ryan <ryandemmer@gmail.com>
2016-06-23 10:16:06 +0100

    Add support for profile by id

--- components/com_jce/editor/libraries/classes/editor.php
+++ components/com_jce/editor/libraries/classes/editor.php
@@ -2,7 +2,7 @@
 
 /**
  * @package   	JCE
- * @copyright 	Copyright (c) 2009-2016 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright (c) 2009-2015 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -10,6 +10,8 @@
  * other free or open source software licenses.
  */
 defined('_JEXEC') or die('RESTRICTED');
+
+require_once(JPATH_ADMINISTRATOR . '/components/com_jce/includes/base.php');
 
 /**
  * JCE class
@@ -22,13 +24,13 @@ class WFEditor extends JObject {
 
     // Editor version
     protected $_version = '@@version@@';
-    
-    // Editor instance 
+
+    // Editor instance
     protected static $instance;
-    
+
     // Editor Profile
     protected static $profile;
-    
+
     // Editor Params
     protected static $params = array();
 
@@ -79,18 +81,7 @@ class WFEditor extends JObject {
             $user = JFactory::getUser();
             $option = $this->getComponentOption();
 
-            $query = $db->getQuery(true);
-
-            if (is_object($query)) {
-                $query->select('*')->from('#__wf_profiles')->where('published = 1')->order('ordering ASC');
-            } else {
-                $query = 'SELECT * FROM #__wf_profiles'
-                        . ' WHERE published = 1'
-                        . ' ORDER BY ordering ASC';
-            }
-
-            $db->setQuery($query);
-            $profiles = $db->loadObjectList();
+            $profile_id = 0;
 
             if ($option == 'com_jce') {
                 $component_id = JRequest::getInt('component_id');
@@ -99,10 +90,35 @@ class WFEditor extends JObject {
                     $component = WFExtensionHelper::getComponent($component_id);
                     $option = isset($component->element) ? $component->element : $component->option;
                 }
+
+                $profile_id = JRequest::getInt('profile_id');
             }
+
+            $query = $db->getQuery(true);
+
+            if (is_object($query)) {
+                $query->select('*')->from('#__wf_profiles')->where('published = 1')->order('ordering ASC');
+
+                if ($profile_id) {
+                    $query->where('id = ' . (int) $profile_id);
+                }
+
+            } else {
+                $query = 'SELECT * FROM #__wf_profiles WHERE published = 1';
+
+                if ($profile_id) {
+                    $query .= ' AND id = ' . (int) $profile_id;
+                }
+
+                $query .= ' ORDER BY ordering ASC';
+            }
+
+            $db->setQuery($query);
+            $profiles = $db->loadObjectList();
+
             // get the Joomla! area (admin or site)
             $area = $mainframe->isAdmin() ? 2 : 1;
-            
+
             if (!class_exists('Wf_Mobile_Detect')) {
                 // load mobile detect class
                 require_once(dirname(__FILE__) . '/mobile.php');
@@ -168,7 +184,7 @@ class WFEditor extends JObject {
                     wfimport('admin.models.editor');
                     $model      = new WFModelEditor();
                     $plugins    = (array) $model->getPlugins();
-                    
+
                     if (in_array($plugin, $plugins) === false) {
                         continue;
                     }
@@ -178,10 +194,10 @@ class WFEditor extends JObject {
                     wfimport('admin.helpers.encrypt');
                     $item->params = WFEncryptHelper::decrypt($item->params);
                 }
-                
+
                 // assign item to profile
                 self::$profile = $item;
-                
+
                 // return
                 return self::$profile;
             }
@@ -206,11 +222,11 @@ class WFEditor extends JObject {
                 break;
             case 'com_categories' :
                 $section = JRequest::getCmd('section');
-                
+
                 if ($section) {
                     $option = $section;
                 }
-                
+
                 break;
         }
 
@@ -242,15 +258,15 @@ class WFEditor extends JObject {
         if ($plugin) {
             $options['plugin'] = $plugin;
         }
-        
+
         $signature = serialize($options);
 
         if (empty(self::$params[$signature])) {
             wfimport('admin.helpers.extension');
-            
+
             // get plugin
             $editor_plugin = WFExtensionHelper::getPlugin();
-            
+
             // get params data for this profile
             $profile = $this->getProfile($plugin);
 
@@ -263,7 +279,7 @@ class WFEditor extends JObject {
             } else {
                 // get component
                 $component = WFExtensionHelper::getComponent();
-                
+
                 // get params from component "params" field (legacy)
                 if ($component->params && $component->params !== "{}") {
                     $data = json_decode($component->params, true);
@@ -277,17 +293,17 @@ class WFEditor extends JObject {
             if ($profile) {
                 $profile_params = json_decode($profile->params, true);
             }
-            
+
             // make sure we have an empty array if null or false
             if (empty($editor_params)) {
                 $editor_params = array();
             }
-            
+
             // make sure we have an empty array if null or false
             if (empty($profile_params)) {
                 $profile_params = array();
             }
-            
+
             // merge data and convert to json string
             $data = WFParameter::mergeParams($editor_params, $profile_params, true, false);
 
