01a45cc0852597b197c863455d6f292454f5173b
Ryan <ryandemmer@gmail.com>
2016-06-23 10:11:18 +0100

    Rename browser.js to compat.js

--- components/com_jce/editor/libraries/js/lib/filebrowser/compat.js
+++ components/com_jce/editor/libraries/js/lib/filebrowser/compat.js
@@ -8,107 +8,75 @@
  * other free or open source software licenses.
  */
 var WFFileBrowser = {
-	
+
     settings 	: {},
-	
+
     element 	: '',
-	
+
     /**
 	 * Initialize FileBrowser
 	 * @param element Mixed Element or Element Selector
 	 * @params options Object
 	 */
     init : function(element, options) {
-        $.extend(true, this.settings, options);	
-		
-        this.element = element;		
-        this._createBrowser();
+				var self = this;
+
+				$.extend(true, this.settings, options);
+
+				this.element = element;
+				$(this.element).filebrowser(this.settings);
     },
-	
-    _createBrowser : function() {
-        $(this.element).MediaManager(this.settings);
-    },
-	
+
     /**
 	 * Get the base directory
 	 */
     getBaseDir : function() {
-        return this._call('getBaseDir');
+        return $.fn.filebrowser.getbasedir();
     },
-	
+
     /**
 	 * Get current directory
 	 */
     getCurrentDir : function() {
-        return this._call('getCurrentDir');
+        return $.fn.filebrowser.getcurrentdir();
     },
-	
+
     /**
 	 * Get a list of selected items optionally fitlered by key
 	 */
     getSelectedItems : function(key) {
-        return this._call('getSelectedItems', key);
+        return $.fn.filebrowser.getselected(key);
     },
-        
+
     /**
 	 * Set a list of selected items
 	 */
     setSelectedItems : function(items) {
-        return this._call('setSelectedItems', items);
+				return $.fn.filebrowser.setselected(items);
     },
-	
+
     /**
 	 * Refresh the browser
 	 */
     refresh : function() {
-        return this._call('refresh');
+        return $.fn.filebrowser.refresh();
     },
-	
-    error : function(error) {
-        return this._call('error', error);
-    },
-	
+
     status : function(message, state) {
-        return this._call('setStatus', {
-            message : message, 
-            state : state
-        });
+				return $.fn.filebrowser.status(message, state);
     },
-	
+
     /**
 	 * Load the browser and set optional return items to select
 	 */
     load : function(items) {
-        return this._call('load', items);
+				$(this.element).trigger('filebrowser:load', items);
     },
-	
-    resize : function(fh) {
-        return this._call('resize', [null, fh]);
-    },
-	
-    startUpload : function() {
-        return this._call('startUpload');
-    },
-	
-    stopUpload : function() {
-        return this._call('stopUpload');
-    },
-	
-    setUploadStatus : function(message, state) {
-        return this._call('setUploadStatus', {
-            message : message, 
-            state : state
-        });
-    },
-	
+
     /**
 	 * Compatability function
 	 */
     get : function(fn, args) {
-        return this._call(fn, args);
-    },
-	
-    _call : function(fn, args) {
-        return $(this.element).MediaManager(fn, args);
+        return WFFileBrowser[fn](args);
     }
 };
